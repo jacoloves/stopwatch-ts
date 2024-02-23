0eefd84fe1641af73fdfe82cf0c562be93050503
shooonng <5511068t@gmail.com>
2024-02-24 08:53:47 +0900

    add rap button and stop button

--- stopwatch.ts
+++ stopwatch.ts
@@ -1,10 +1,15 @@
 let timerId: number | undefined;
-let time = 0;
+let elapsed: number = 0;
+let startTime: number | undefined;
 const timerDisplay = document.getElementById('timer') as HTMLDivElement;
 const startStopBtn = document.getElementById('startStopBtn') as HTMLButtonElement;
 const resetBtn = document.getElementById('resetBtn') as HTMLButtonElement;
+const lapBtn = document.getElementById('lapBtn') as HTMLButtonElement;
 
 function updateTimeDisplay() {
+  const now = performance.now();
+  const time = elapsed + (startTime ? now - startTime : 0);
+
   const milliseconds = time % 1000;
   const seconds = Math.floor((time / 1000) % 60);
   const minutes = Math.floor((time / 60000) % 60);
@@ -13,28 +18,42 @@ function updateTimeDisplay() {
 }
 
 function startTimer() {
-  if (!timerId) {
-    timerId = setInterval(() => {
-      time += 10;
-      updateTimeDisplay();
-    }, 10);
+  if (!startTime) {
+    startTime = performance.now() - elapsed;
+    timerId = requestAnimationFrame(updateTime);
     startStopBtn.textContent = 'Stop';
+    resetBtn.disabled = true;
+    lapBtn.disabled = false;
   } else {
-    clearInterval(timerId);
-    timerId = undefined;
+    elapsed += performance.now() - startTime;
+    cancelAnimationFrame(timerId);
+    startTime = undefined;
     startStopBtn.textContent = 'Start';
+    resetBtn.disabled = false;
+    lapBtn.disabled = true;
   }
 }
 
+function updateTime() {
+  updateTimeDisplay();
+  timerId = requestAnimationFrame(updateTime);
+}
+
 function resetTimer() {
-  clearInterval(timerId);
-  timerId = undefined;
-  time = 0;
+  cancelAnimationFrame(timerId);
+  startTime = undefined;
+  elapsed = 0;
   updateTimeDisplay();
   startStopBtn.textContent = 'Start';
+  resetBtn.disabled = false;
+  lapBtn.disabled = true;
 }
 
 startStopBtn.addEventListener('click', startTimer);
 resetBtn.addEventListener('click', resetTimer);
 
 updateTimeDisplay();
+
+window.onload = () => {
+  lapBtn.disabled = true;
+};
